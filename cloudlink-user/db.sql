d76b86ae22e40f37c56336343df9f10f0b2f1699
xiefayang <coderthank@163.com>
2019-04-12 14:52:15 +0800

    feat: update project as a parent-child maven project

--- cloudlink-user/db.sql
+++
@@ -1,8 +0,0 @@
-create table `pri_user` (
-  `id` varchar(64) not null,
-  `name` varchar(64) not null comment '姓名',
-  `age` smallint not null comment '年龄',
-  `create_time` timestamp not null default current_timestamp comment '创建时间',
-  `update_time` timestamp not null default current_timestamp on update current_timestamp comment '修改时间',
-  primary key (`id`)
-);
